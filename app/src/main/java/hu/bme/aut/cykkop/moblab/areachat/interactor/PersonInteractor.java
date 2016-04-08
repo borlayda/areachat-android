39a4c6b249dfd01c00a72c930ba8aefe89aad6c4
Borlay Dániel <borlay.daniel@gmail.com>
2016-04-08 11:15:12 +0200

    Dagger usage and Singleton Interactors

--- app/src/main/java/hu/bme/aut/cykkop/moblab/areachat/interactor/PersonInteractor.java
+++ app/src/main/java/hu/bme/aut/cykkop/moblab/areachat/interactor/PersonInteractor.java
@@ -1,7 +1,10 @@
 package hu.bme.aut.cykkop.moblab.areachat.interactor;
 
+import javax.inject.Singleton;
+
 /**
  * Created by mobsoft on 2016. 04. 08..
  */
+@Singleton
 public class PersonInteractor {
 }
--- app/src/main/java/hu/bme/aut/cykkop/moblab/areachat/interactor/Presenter.java
+++ app/src/main/java/hu/bme/aut/cykkop/moblab/areachat/interactor/Presenter.java
@@ -1,7 +1,20 @@
 package hu.bme.aut.cykkop.moblab.areachat.interactor;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 /**
  * Created by mobsoft on 2016. 04. 08..
  */
+@Singleton
 public class Presenter {
+
+    @Inject
+    private AuthInteractor authInteractor;
+    @Inject
+    private MessageInteractor messageInteractor;
+    @Inject
+    private PositionInteractor positionInteractor;
+    @Inject
+    private PersonInteractor personInteractor;
 }
